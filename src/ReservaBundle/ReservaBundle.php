1ea2c7317054dc4212831d353928f473ab659f34
rr14059 <rr14059@ues.edu.sv>
2018-10-27 20:04:49 -0600

    Creacion de bundles ventas y reserva

---
+++ src/ReservaBundle/ReservaBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace ReservaBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class ReservaBundle extends Bundle
+{
+}
---
+++ src/VentaBundle/VentaBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace VentaBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class VentaBundle extends Bundle
+{
+}
